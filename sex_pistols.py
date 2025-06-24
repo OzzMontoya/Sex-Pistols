702c6aac339a091440649b55decc76f3ab38f2fa
Kiot-Cat <deotc1170@gmail.com>
2025-06-24 14:59:55 -0600

    Gael_Puto

--- sex_pistols.py
+++ sex_pistols.py
@@ -1,1 +1,13 @@
-sex_pistolsS
+#sex_pistolsS
+
+
+import tkinter as tk
+
+ventana = tk.Tk()
+ventana.title("🔥")
+ventana.geometry("300x150")
+
+etiqueta = tk.Label(ventana, text="Gael puto 🗣️🗣️🔥🔥")
+etiqueta.pack()
+
+ventana.mainloop()
